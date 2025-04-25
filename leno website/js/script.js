9fd73c170e97cd38203aaec4a2da447a68a7f82c
Quazi Fariha Fairooz <91182471+Qfariha@users.noreply.github.com>
2025-04-25 12:46:04 +0600

    L-186

---
+++ leno website/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Mobile Menu
+    const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+  
+    toggleButton.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active');
+        });
+});
+    
+// Nav scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+  
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar--scroll');
+    } else {
+      navbar.classList.remove('navbar--scroll');
+    }
+  });
+  
